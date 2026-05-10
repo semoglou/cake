5c8d2f82a425aca241ee356f9e5826f42cd2f758
Angelos Semoglou <a.semoglou@outlook.com>
2026-05-10 15:36:55 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cake-ensemble",
-    version="0.1.0",
+    version="0.1.1",
     author="Aggelos Semoglou",
     author_email="a.semoglou@outlook.com",
     description="CAKE: Confidence in Assignments via K-partition Ensembles",
